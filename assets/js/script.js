2dd3671c909046c0178a8bde52e36e716fc0a842
priyashanmugam2512 <priyashanmugam2512@gmail.com>
2024-05-03 10:14:42 +0530

    landing page code updated

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $('.brand_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          arrows:false,
+          dots:true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          arrows:false,
+          dots:true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          arrows:false,
+          dots:true
+        }
+      }
+    ]
+  });
+});
